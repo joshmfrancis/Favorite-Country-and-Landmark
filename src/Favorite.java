24008a03f22a8bf00afc6c394304bb083ba46eb9
Joshua Middleton Francis <Thejoshuafrancis@gmail.com>
2022-05-09 01:17:13 -0500

    Uploaded Files

---
+++ src/Favorite.java
@@ -0,0 +1,18 @@
+// Name: Joshua Francis
+// Course: CSC 135
+// Project: Problem_1_1
+// File Name: Favorite.java
+
+// This program prints my favorite country and two landmarks in that country.
+public class Favorite {
+
+    public static void main(String[] args) {
+
+        System.out.println("Country: New Zealand");                                       // Prints my favorite country
+
+        System.out.println("Landmarks: Hobbiton Movie Set, Rotorua Hot Springs");         // Prints two landmarks in the country above   
+        System.out.println("I said \"Hello\" to you.");
+
+    }
+
+}
